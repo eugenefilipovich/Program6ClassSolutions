1ec9eda889281faf15fe56babc29b3e1b882e337
Eugene Filipovich <eugenefilipovich@email.com>
2015-03-18 15:58:14 -0600

    Day 38, Class

---
+++ Week7CodeChallenge/Week7CodeChallenge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week7CodeChallenge.Startup))]
+namespace Week7CodeChallenge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
